730bd587148579f8170b01ea7efafba8b1943e79
fernando.h.alves <fernando.h.alves@kroton.com.br>
2019-01-08 15:10:36 -0200

    Add @Query EventRepository

--- src/main/java/com/hackerrank/github/repository/EventRepository.java
+++ src/main/java/com/hackerrank/github/repository/EventRepository.java
@@ -1,11 +1,13 @@
 package com.hackerrank.github.repository;
 
 import com.hackerrank.github.model.Event;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface EventRepository extends CrudRepository<Event, Long> {
 
+    @Query("select e from Event e where e.actor.id = ?1")
     List<Event> getByActorId(Integer actorId);
 }
